d017c7754d5b2d8952606ec6f1357ba11b3b89f3
Outerstock <k_rasit@kkumail.com>
2020-04-26 22:57:59 +0700

    fortune 2

---
+++ js/myfunction.js
@@ -0,0 +1,12 @@
+function myfunction() {
+    document.write("from my function <br>")
+}
+
+function call(num1,num2) {
+    var x = num1 + num2
+    document.write(x)
+}
+
+function call2(txt1,txt2) {
+    return txt1 + ' ' + txt2
+}
